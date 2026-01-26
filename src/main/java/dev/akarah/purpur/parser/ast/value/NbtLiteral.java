9cb263cdb6cae59059c876164ab9331ea73c5821
akarahdev <endisticbutitsforgit@gmail.com>
2026-01-26 18:42:51 -0500

    fix: nbt literals now render as proper snbt

--- src/main/java/dev/akarah/purpur/parser/ast/value/NbtLiteral.java
+++ src/main/java/dev/akarah/purpur/parser/ast/value/NbtLiteral.java
@@ -7,13 +7,14 @@ import dev.akarah.purpur.parser.ast.AST;
 import dev.dfonline.flint.actiondump.codeblocks.ActionType;
 import dev.dfonline.flint.templates.argument.NumberArgument;
 import dev.dfonline.flint.templates.argument.abstracts.Argument;
+import net.minecraft.data.structures.SnbtToNbt;
 import net.minecraft.nbt.*;
 
 public record NbtLiteral(Tag tag, SpanData spanData) implements Value {
     @Override
     public void lowerToParsable(StringBuilder builder, int depth) {
         builder.append("nbt(");
-        NbtUtils.prettyPrint(builder, tag, depth, true);
+        builder.append(NbtUtils.structureToSnbt((CompoundTag) tag));
         builder.append(")");
     }
 
